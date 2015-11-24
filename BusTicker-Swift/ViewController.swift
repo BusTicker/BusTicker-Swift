b76fb0fc7b64a689e4ed12b60cd6700dd296a67a
brennanMKE <brennan@smallsharptools.com>
2015-11-24 14:51:10 -0600

    init

---
+++ BusTicker-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusTicker-Swift
+//
+//  Created by Brennan Stehling on 11/24/15.
+//  Copyright © 2015 SmallSharpTools LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
